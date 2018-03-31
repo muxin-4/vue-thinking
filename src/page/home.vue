6c20e9ed13990e687e2a799e0dfabb1f16d501af
wgy <952046097@qq.com>
2018-03-31 19:55:37 +0800

    feat: init

---
+++ src/page/home.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div :class="$style.home">
+        <h1>Home</h1>
+        <a>asdb</a>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" module>
+  .home{
+    color: red;
+    // font-size: 80px;
+  }
+</style>

eee4b9ccbd36685cf4b89f037d27df15926fc082
wgy <952046097@qq.com>
2018-04-01 19:55:25 +0800

    feat：功能级别模块 面板

---
+++ src/components/core/panel.vue
@@ -0,0 +1,41 @@
+<template>
+  <section :class="[panelClass, canme]">
+    <h4>-{{ title }}-</h4>
+    <div>{{ content }}</div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    cname: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: "",
+    }
+  },
+  data () {
+    return {
+      panelClass: "panel",
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+@import '@/css/element.scss';
+.panel {
+  @include panel;
+}
+</style>

bb1efbf66aa396320bd0b16e177228ec3f7e6700
wgy <952046097@qq.com>
2018-04-12 22:24:49 +0800

    feat: 功能组件 页脚

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -5,11 +5,13 @@
     <FinancingChoice />
     <Product />
     <Life />
+    <Footor />
   </div>
 </template>
 
 <script>
 import Heador from '@/components/public/header.vue'
+import Footor from '@/components/public/footer.vue'
 import Slider from './hslider.vue'
 import FinancingChoice from './financing-choice.vue'
 import Product from './product.vue'
@@ -18,6 +20,7 @@ import Life from './life.vue'
 export default {
   components: {
     Heador,
+    Footor,
     Slider,
     FinancingChoice,
     Product,

00d49bb3a3e68e68c015eaf0c531fc2263073edd
wgy <952046097@qq.com>
2018-04-14 00:06:09 +0800

    feat：理财页面

---
+++ src/page/money/index.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div>
+    <Heador />
+    <Slider />
+    <Ada />
+    <FinancingChoice />
+    <Adb />
+    <Footor />
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Heador from '@/components/public/header.vue'
+import Footor from '@/components/public/footer.vue'
+import Slider from './mslider.vue'
+import FinancingChoice from './financing-choice.vue'
+import Ada from './ada.vue'
+import Adb from './adb.vue'
+import Navbar from '@/components/public/navbar.vue'
+
+export default {
+  components: {
+    Heador,
+    Footor,
+    Navbar,
+    Slider,
+    Ada,
+    Adb,
+    FinancingChoice
+  }
+}
+</script>
+
+<style lang="scss" module>
+</style>

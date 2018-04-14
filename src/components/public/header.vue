13b5b99d91de65362945a31f8b633071c2e412a6
wgy <952046097@qq.com>
2018-04-14 10:40:44 +0800

    fix: 页头背景图片透明

--- src/components/public/header.vue
+++ src/components/public/header.vue
@@ -1,54 +1,67 @@
 <template lang='html'>
   <div :class="$style.header">
-    <span :class="$style.left">
-      <em>注册</em>&nbsp;|&nbsp;<em>登录</em>
+    <div :class="$style.wrap">
+      <span :class="$style.left">
+        <em>注册</em>&nbsp;|&nbsp;<em>登录</em>
+      </span>
+      <img src="//m.jr.jd.com/spe/qyy/main/images/jr-logo.png" alt="">
       <btn :class="$style.btnDownload">APP下载</btn>
-    </span>
+    </div>
   </div>
 </template>
 
 <script>
 import btn from '../core/btn.vue'
 export default {
-  components: {
-    btn
-  },
-  data () {
-    return {
-    }
-  }
+	components: {
+		btn,
+	},
+	data() {
+		return {}
+	},
 }
 </script>
 <style lang='scss' module>
+@import '../../css/element.scss';
 .header {
-  color: #666;
-  height: 100px;
-  line-height: 100px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  font-size: 32px;
-  background: #fff url(//m.jr.jd.com/spe/qyy/main/images/jr-logo.png) center center no-repeat;
-  background-size: auto 50%;
+	color: #666;
+	height: 100px;
+	line-height: 100px;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	font-size: 32px;
+	background-color: #fff;
+	z-index: 999;
 
-  .left {
-    font-size: 28px;
-    height: 30px;
-    line-height: 30px;
-    margin: 17px 0 0 18px;
-    color: #666;
-  }
-  .btnDownload {
-    float: right;
-    font-size: 24px;
-    border-width: 0;
-    height: 56px;
-    line-height: 56px;
-    min-width: 120px;
-    padding: 0;
-    border-radius: 4px;
-    margin: 28px 24px 0 0;
-  }
+	.wrap {
+    @include flex(row);
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+		.left {
+			font-size: 28px;
+			height: 30px;
+			line-height: 30px;
+      color: #666;
+      margin: 16px 0 0 24px;
+		}
+		img {
+			width: 210px;
+			margin: 9px auto;
+			display: block;
+		}
+		.btnDownload {
+			font-size: 24px;
+			border-width: 0;
+			height: 56px;
+			line-height: 56px;
+			min-width: 120px;
+			padding: 0;
+      border-radius: 4px;
+      margin-right: 24px;
+		}
+	}
 }
 </style>

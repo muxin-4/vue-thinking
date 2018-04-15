5ea793d1e557d7cb4a8b31c79e35db075fa3ee1f
wgy <952046097@qq.com>
2018-04-15 10:11:40 +0800

    feat：个人中心路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,10 @@ import Router from 'vue-router'
 import Home from '@/page/home/index.vue'
 import Money from '@/page/money/index.vue'
 import Ious from '@/page/ious/index.vue'
+import Account from '@/page/account/index.vue'
+import Download from '../specail/download.vue'
 import '../css/reset.scss'
+import '../css/config.scss'
 
 Vue.use(Router)
 
@@ -24,5 +27,15 @@ export default new Router({
       name: 'ious',
       component: Ious
     },
+    {
+      path: '/account',
+      name: 'account',
+      component: Account
+    },
+    {
+      path: '/download',
+      name: 'download',
+      component: Download
+    }
   ]
 })

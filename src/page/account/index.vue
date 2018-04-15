fe27b025e8acb167716aaf8586cf24d5ade69efa
wgy <952046097@qq.com>
2018-04-15 10:11:05 +0800

    feat：个人中心

---
+++ src/page/account/index.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div>
+    <Heador />
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Heador from '@/components/public/header.vue'
+import Navbar from '@/components/public/navbar.vue'
+
+
+export default {
+  components: {
+    Heador,
+    Navbar,
+
+  }
+}
+</script>
+
+<style lang="scss" module>
+</style>
